ea005dd97f063520cbab07814dd20e5ad7c89415
Vladimir Malygin <62510787+malyginvladimir@users.noreply.github.com>
2021-11-10 12:31:39 +0100

    Update download_and_process.py

--- download_and_process.py
+++ download_and_process.py
@@ -65,7 +65,7 @@ def downloadAllBinance(symbol, kline_size, save=False):
 
 def find_symbol_filenames(directory_to_raw_data, 
                           tickers_to_process = None, 
-                          base_ticker = "USDT")
+                          base_ticker = "USDT"):
     
     """
     The function is created to find necessary price data among a variety
@@ -142,124 +142,6 @@ def generate_dates_vector(start_date, end_date, step = 60):
     vectorDates = pd.DataFrame(np.asanyarray(vectorDates, dtype='datetime64'))
     vectorDates.rename(columns = {0: "Date"}, inplace = True)
     return(vectorDates)
-
-def find_symbol_filenames(directory_to_raw_data, 
-                          tickers_to_process = None, 
-                          base_ticker = "USDT")
-    
-    """
-    The function is created to find necessary price data among a variety
-    of cryptopairs in `directory_to_raw_data`. It is possible to specify
-    cryptopairs via `tickers_to_process` or define `base_ticker` which is used
-    to find all cryptopairs that are traded to `base_ticker` (e.g., given
-    `base_ticker` = "USDT", the "BTCUSDT" pair will be found). 
-    The function requires that filenames that are stored in `directory_to_raw_data`
-    to follow the following name pattern: "Ticker.csv". For instance,
-    "BTCUSDT.csv".
-    
-    Arguments:
-        directory_to_raw_data -- string, path to directory where price data is stored
-        tickers_to_process -- list, the data for the tickers in the list will be searched (e.g., ["BTCUSDT", "XRPBTC"])
-        base_ticker -- string, all pairs associated with this ticker will be searched (e.g., ["BTCUSDT", "ETHUSDT"])
-        base_ticker = "USDT"
-    Returns:
-        tickers_to_process -- list of tickers that were searched (auxiliary)
-        selected_file_names -- list of filenames found
-    """
-    
-    path = directory_to_raw_data + "*" + ".csv"
-    all_file_names = glob.glob(path)
-    all_tickers = [file_name.split('-')[0].split('/')[-1] for file_name in all_file_names]
-    usdt_tickers = []
-    btc_tickers = []
-    for i in range(0, len(all_tickers)):
-        try:
-            if(isinstance(all_tickers[i].index("USDT"), int) == True):
-                usdt_tickers.append(all_tickers[i])
-        except:
-            if(isinstance(all_tickers[i].index("BTC"), int) == True):
-                btc_tickers.append(all_tickers[i])
-    if (tickers_to_process == None) & (base_ticker == "USDT"):
-        tickers_to_process = usdt_tickers
-        
-    elif (tickers_to_process == None) & (base_ticker == "BTC"):
-        tickers_to_process = btc_tickers
-    
-    selected_file_names =[directory_to_raw_data
-                             + ticker
-                             + '.csv'
-                             for ticker in tickers_to_process]
-    return(tickers_to_process, selected_file_names)
-    
-def create_OCHLVT_tables(start_date, end_date, 
-                         step, directory_to_raw_data, 
-                         export_directory, 
-                         tickers_to_process = None, 
-                         base_ticker = "USDT"):
-    directory_to_raw_data = directory_to_raw_data
-    columnIndexes = [1, 2, 3, 4, 5, 8]
-    columnNames = ['open', 'high', 'low', 'close', 'volume', 'trades']
-    vectorDates = generate_dates_vector(start_date = start_date,
-                                        end_date = end_date, step = step)
-                                        
-    tickers, relevant_file_names  = find_symbol_filenames(directory_to_raw_data, 
-                                                          tickers_to_process, 
-                                                          base_ticker)
-    fileNames = relevant_file_names
-    iterations = len(columnIndexes)
-    timeElapsed = []
-    coinFlag = base_ticker
-    indexForColumnNames = 0
-    for columnIndex in columnIndexes:
-        startTimer = time.time()
-        finalTable = None
-        finalTable = np.empty(shape=(len(vectorDates), len(fileNames)),
-                              dtype='float')
-        i = 0
-        for fileName in fileNames:
-            dataFrame = pd.read_csv(fileName, usecols=[0, columnIndex])
-            ochlvFlag = dataFrame.columns[1]
-            targetColumn = np.asanyarray(dataFrame.iloc[:, 1], dtype='float')
-            timeStampsVector = np.asanyarray(dataFrame['timestamp'],
-                                             dtype='datetime64')
-            foundTimeStamps, indexIntersectBasis, indexIntersectLocal = \
-                np.intersect1d(vectorDates, timeStampsVector, return_indices=True)
-            finalTable[indexIntersectBasis, i] = targetColumn[indexIntersectLocal]
-            i += 1
-        finalTable[finalTable == 0] = np.nan
-        finalTableDataFrame = pd.DataFrame(finalTable)
-        finalTableDataFrame = pd.concat([vectorDates,
-                                         finalTableDataFrame], axis=1)
-        finalTableDataFrame.columns = ['Date'] + tickers
-        star_date_name = str(start_date.year) + "-" \
-                         + str(start_date.month) + "-" \
-                         + str(start_date.day)
-        end_date_name = str(end_date.year) + "-" \
-                        + str(end_date.month) + "-" \
-                        + str(end_date.day)
-        fileNameToWrite = star_date_name + '_' \
-                          + end_date_name + '-' \
-                          + ochlvFlag + '-' \
-                          + coinFlag + '.csv'
-
-        finalTableDataFrame.to_csv(export_directory + fileNameToWrite, index=False)
-        endTimer = time.time()
-        timeElapsedPoint = endTimer - startTimer
-        iterations += -1
-        timeElapsed.append(timeElapsedPoint)
-        expetedTimeLeftMinutes = round(Average(timeElapsed) * iterations / 60, 2)
-        finalTableDataFrame = None
-        indexForColumnNames += 1
-
-        print(ochlvFlag + " data" + " has been generated. " + str(iterations)
-              + " files to generate left. " + "Expected time to complete: "
-              + str(expetedTimeLeftMinutes) + " minutes.")
-
-        print('The name of the generated file is ' + fileNameToWrite + '.')
-        print()
-
-    return()
-
  
 tickers, filenames  = find_symbol_filenames(directory_to_raw_data = '')
   
@@ -268,7 +150,7 @@ timeElapsed = [] # stores time elampsed for each item
 tickers_missed = [] # stores the tickers that were not dowloaded for troubleshoting
 numOfTickers = len(tickers) # total number of tickers to download
 iterations = numOfTickers # iterations left
-freq = "1m" # data frequency
+freq = "1m" # data frequency {"1m": 1, "5m": 5, "1h": 60, "1d": 1440}
 
 for symbol in tickers:
 
